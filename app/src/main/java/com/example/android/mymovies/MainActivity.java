0786d409f9907d33bcf4945d4b757900dfdab591
SIPADALA <padalasivakumar5@gmail.com>
2018-02-25 10:05:51 +0530

    added loader calls

--- app/src/main/java/com/example/android/mymovies/MainActivity.java
+++ app/src/main/java/com/example/android/mymovies/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.android.mymovies;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,20 +19,111 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+import static android.text.TextUtils.isEmpty;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     private GridView myMovieGrid;
     private final String TOP_RATED = "top_rated";
 
+    private final String TOP_RATED_KEY = "top_rated";
+
+    private static final int MOVIE_FETCH_LOADER = 99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MyMovieAsyncTask myStr = new MyMovieAsyncTask();
-        myStr.execute(TOP_RATED);
+
+
+        /*MyMovieAsyncTask myStr = new MyMovieAsyncTask();
+        myStr.execute(TOP_RATED);*/
+
+        Log.v("TAG1","Before initializung");
+
+        //getSupportLoaderManager().initLoader(MOVIE_FETCH_LOADER, null, this);
+        dbFetch(TOP_RATED);
+        Log.v("TAG1","Afetr initializung");
+        //dbFetch(TOP_RATED);
+
     }
 
-    private class MyMovieAsyncTask extends AsyncTask<String,Void, String>{
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        return  new AsyncTaskLoader<String>(this) {
+
+            String jsonRes;
+            @Override
+            protected void onStartLoading() {
+                Log.v("TAG1","onStartLoading");
+                if (args == null)
+                {
+                    Log.v("TAG1","onStartLoading is null");
+                    return;
+                }
+                if (jsonRes != null) {
+                    deliverResult(jsonRes);
+                } else {
+                    forceLoad();
+                }
+                Log.v("TAG1",args.getString(TOP_RATED_KEY));
+
+            }
+
+            @Override
+            public String loadInBackground() {
+                Log.v("TAG1","loadInBackground");
+                String myRating = args.getString(TOP_RATED_KEY);
+                if(myRating == null || TextUtils.isEmpty(myRating)){
+                    return null;
+                }
+                try {
+                    Log.v("TAG1","loadInBackground try ");
+                    HttpRequest myMovieReq = new HttpRequest(myRating);
+                    return myMovieReq.getJsonString();
+                }catch (Exception e){
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            public void deliverResult(String data) {
+                jsonRes = data;
+                super.deliverResult(data);
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, final String data) {
+
+        Log.v("TAG1","onLoadFinished");
+
+        MovieDetails myMovieList = new MovieDetails(data);
+        ArrayList<String> myArrList = myMovieList.getMovieList();
+        MovieAdapter myMovieAdapter  = new MovieAdapter(getApplicationContext(),myArrList);
+        myMovieGrid = findViewById(R.id.my_movie_grid);
+        myMovieGrid.setAdapter(myMovieAdapter);
+        myMovieGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                //Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
+                HashMap<String,String> myMovie;
+                MovieDetails movieDetails = new MovieDetails(data);
+                myMovie = movieDetails.getMyMovie(position);
+                Intent intent = new Intent(getApplicationContext(), MovieIntentActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT,myMovie);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+   /* private class MyMovieAsyncTask extends AsyncTask<String,Void, String>{
         @Override
         protected String doInBackground(String... strings) {
             HttpRequest myMovieReq = new HttpRequest(strings[0]);
@@ -56,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
             });
             super.onPostExecute(str);
         }
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,20 +161,43 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void dbFetch(String order){
+
+        Log.v("TAG1","IN dbFetch");
+        Bundle myBundle = new Bundle();
+        myBundle.putString(TOP_RATED_KEY,order);
+
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<String> movieDbLoader = loaderManager.getLoader(MOVIE_FETCH_LOADER);
+
+        if (movieDbLoader == null) {
+            Log.v("TAG1","loader is null");
+            loaderManager.initLoader(MOVIE_FETCH_LOADER, myBundle, this);
+        } else {
+            Log.v("TAG1","loader noit null");
+            loaderManager.restartLoader(MOVIE_FETCH_LOADER, myBundle, this);
+        }
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         myMovieGrid = findViewById(R.id.my_movie_grid);
-        MyMovieAsyncTask myStr = new MyMovieAsyncTask();
+        Log.v("TAG1","Before on Opytions");
+        //dbFetch(TOP_RATED);
+        Log.v("TAG1","After on Opt");
 
         switch(item.getItemId()){
             case R.id.top_rated_movie:
                 //Toast.makeText(getApplicationContext(),"Top Rated",Toast.LENGTH_SHORT).show();
-                myStr.execute(TOP_RATED);
+                Log.v("TAG1","Top rated");
+                dbFetch(TOP_RATED);
                 return true;
             case  R.id.popular_movie:
                 //Toast.makeText(getApplicationContext(),"Popular",Toast.LENGTH_SHORT).show();
                 String POPULAR = "popular";
-                myStr.execute(POPULAR);
+                Log.v("TAG1","Popular");
+                dbFetch(POPULAR);
                 return true;
             default:
                 Toast.makeText(getApplicationContext(),"Default",Toast.LENGTH_SHORT).show();
